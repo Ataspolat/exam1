3d1274fd12456eacfd8e54d4867d5f9aa9e45c34
Ataspolat <ataaas6250@gmail.com>
2024-06-30 10:40:05 +0500

    <1.4.1>

--- database/migrations/2024_06_29_190423_create_teachers_table.php
+++ database/migrations/2024_06_29_190423_create_teachers_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->first_name();
+            $table->last_name();
+
         });
     }
 
--- database/migrations/2024_06_29_190503_create_students_table.php
+++ database/migrations/2024_06_29_190503_create_students_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->first_name();
+            $table->last_name();
+            $table->phone();
+            $table->birthday();
+            $table->gender();
+            $table->created_at();
         });
     }
 
